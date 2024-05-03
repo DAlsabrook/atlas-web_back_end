51b0d71cff6ce2596500ba7d8b87b52a461d75a9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-03 12:52:30 -0500

    First attempt at currency class and using a validating method for typeerror handling

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+export default class Currency {
+  constructor(code, name) {
+    if (this.validate(code, 'code')) {
+      this._code = code;
+    }
+    if (this.validate(name, 'name')) {
+      this._name = name;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (this.validate(newCode, 'code')) {
+      this._code = newCode;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (this.validate(newName, 'name')) {
+      this._name = newName;
+    }
+  }
+
+  validate(arg, attr) {
+    if (typeof arg === 'string') {
+      return true;
+    } else {
+      if (attr === 'name') {
+        throw new TypeError('Name must be a string');
+      } else if (attr === 'code') {
+        throw new TypeError('Code must be a string');
+      }
+    }
+  }
+}
