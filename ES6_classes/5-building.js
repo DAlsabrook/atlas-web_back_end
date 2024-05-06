1385b0852d6d236546368b74cb4a81d7fcc87e10
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-06 10:50:30 -0500

    removed () from if statement to check method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
   // The constructor takes one argument: sqft
   constructor(sqft) {
     // Check if the method is there. If not throw an error
-    if (this.constructor !== Building && !this.evacuationWarningMessage()) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     // Check if sqft is a number, if not throw an error
