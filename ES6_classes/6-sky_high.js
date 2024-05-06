908900e7342e31440861dc311660fd62676729d9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-06 11:06:24 -0500

    Refactor SkyHighBuilding constructor to use super() for initializing sqft property

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,13 +2,10 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     if (typeof floors !== 'number') {
       throw new TypeError('Floors must be a number');
     }
-    this._sqft = sqft;
+    super(sqft);
     this._floors = floors;
   }
 
