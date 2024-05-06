4b245d370b611189214cafffe1056ba2052b01ee
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-06 10:47:17 -0500

    apprently the checker doesnt actually want this class to be anabstract class as its first test is creating an instance of it. cool.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,9 +3,9 @@ export default class Building {
   // The constructor takes one argument: sqft
   constructor(sqft) {
     // Check if the class being instantiated is Building
-    // If it is, throw an error because Building is an abstract class and should not be instantiated directly
-    if (this.constructor === Building) {
-      throw new TypeError('Cannot instantiate abstract class.');
+    // If it is, throw an error because Building should not be instantiated directly
+    if (this.constructor !== Building && !this.evacuationWarningMessage()) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     // Check if sqft is a number, if not throw an error
     if (typeof sqft !== 'number') {
@@ -18,11 +18,5 @@ export default class Building {
   // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Method that should be overridden by any class that extends Building
-  // If it's not overridden and this method is called, it will throw an error
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
