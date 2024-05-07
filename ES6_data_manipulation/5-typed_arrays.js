cb7f5b8af22c971cb6876e532a6794b394baccd8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 11:25:07 -0500

    linter issues

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   if (position > (length - 1)) {
     throw new Error('Position outside range');
   }
-  let ab = new ArrayBuffer(length); //Arraybuffer() takes in its size in bytes
-  let view = new DataView(ab); //creatte a data view to look at and change ab
+  const ab = new ArrayBuffer(length); // Arraybuffer() takes in its size in bytes
+  const view = new DataView(ab); // creatte a data view to look at and change ab
   view.setInt8(position, value); // set new number in array buffer
   return view;
 }
