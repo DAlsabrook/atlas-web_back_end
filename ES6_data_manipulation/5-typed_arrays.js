92aca40f8c33278aaf4495fa7a3ed532b47470db
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 11:22:13 -0500

    CHANGED TO DATAVIEW. Then input the int8 instead of creating int8 array.

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   let ab = new ArrayBuffer(length); //Arraybuffer() takes in its size in bytes
-  let view = new Int8Array(ab); //creatte a data view to look at and change ab
-  view[position] = value; // set new number in array buffer
-  return ab;
+  let view = new DataView(ab); //creatte a data view to look at and change ab
+  view.setInt8(position, value); // set new number in array buffer
+  return view;
 }
