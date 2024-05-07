d7cc2e34d667622cef356535b00528be0c69bcf9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 11:14:51 -0500

    Adding error handling and changing how we add ints to the buffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > (length - 1)) {
+    throw new Error('Position outside range');
+  }
   let ab = new ArrayBuffer(length); //Arraybuffer() takes in its size in bytes
-  const view = new Int8Array(ab); //creatte a data view to look at and change ab
-  view.setInt8(position, value); // set new number in array buffer
+  let view = new Int8Array(ab); //creatte a data view to look at and change ab
+  view[position] = value; // set new number in array buffer
+  return view;
 }
