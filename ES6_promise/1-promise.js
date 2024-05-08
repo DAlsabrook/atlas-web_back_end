69048d184ee5063bf6a9a380ef557e1f6e0f8f3d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 21:34:56 -0500

    return promise handling bool param

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default function getFullResponseFromAPI(bool) {
+  return new Promise((resolve, reject) => {
+    if (bool === true) {
+      resolve({'status': 200, 'body': 'Success'});
+    } else if (bool === false) {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
