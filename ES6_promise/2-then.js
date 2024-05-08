34366881e9e8ae0d8defcf8909f8e1e3e087f638
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 21:53:42 -0500

    moving return statment into resolve case

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-  }).catch(() => {
-    return new Error();
-  });
-  return { body: 'success', status: 200 };
+    return { body: 'success', status: 200 };
+  }).catch(() => new Error());
 }
