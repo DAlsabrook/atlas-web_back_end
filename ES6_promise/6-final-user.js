531fc4c7970c6053b894ca1c403e50708d44760c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-08 14:24:19 -0500

    refactor to remove using index

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,12 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result, index) => {
-      if (index === 0) { // signUpUser
-        return {
-          status: result.status,
-          value: result.status === 'fulfilled' ? result.value : `User creation error: ${result.reason.message}`,
-        };
-      } else { // uploadPhoto
-        return {
-          status: result.status,
-          value: result.status === 'rejected' ? result.reason.message : `Photo uploaded: ${result.value}`,
-        };
-      }
+    .then((results) => results.map((result) => {
+      return {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason.message,
+      };
     }));
 }
