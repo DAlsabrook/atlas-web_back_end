bd57e0904f15b0dd63abe5fdac17e94bf4876fcf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 21:37:19 -0500

    refactor: Update promise resolution and rejection handling

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(bool) {
   return new Promise((resolve, reject) => {
     if (bool === true) {
-      resolve({'status': 200, 'body': 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else if (bool === false) {
       reject(new Error('The fake API is not working currently'));
     }
