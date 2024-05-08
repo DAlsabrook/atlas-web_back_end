e4f4dd8b3f252075f29db44a4958b1a132e95a80
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-08 14:28:04 -0500

    changing the error message for task 6

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,5 +2,5 @@ export default function uploadPhoto(filename) {
   if (typeof filename !== 'string') {
     throw new Error('Filename must be string');
   }
-  return Promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise.reject(new Error(`Error: ${filename} cannot be processed`));
 }
