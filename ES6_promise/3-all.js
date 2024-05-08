ec2a812b327785849066f64bf37a6f479257bbee
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-08 11:38:43 -0500

    using photo function body for body return

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfilesSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => console.log(`${user.body} ${user.firstName} ${user.lastName}`))
+    .then(([photo, user]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
