195d97c5bd5a2ddd7f53c11fed6c2c025e4f53a1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-08 14:14:45 -0500

    seperate return values by promise result index

--- ES6_promise/test.js
+++ ES6_promise/test.js
@@ -1,4 +1,4 @@
 import handleProfileSignup from './6-final-user.js';
-const result = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
+const result = handleProfileSignup('John', 'Doe', 'Gerald.jpg');
 console.log(result);
 result.then((resultResult) => console.log(resultResult));
