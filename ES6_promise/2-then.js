81ed3dfad889c3fce3c29ca12673829ba26bc484
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 22:01:48 -0500

    return the stupid promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise
+  .then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
-  }).catch(() => {
+  })
+  .catch(() => {
     throw new Error('');
   });
 }
