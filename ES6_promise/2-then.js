4e062d9b1089a7f58da642c6ce15c08ec45dc328
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-07 21:57:12 -0500

    fixed error handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { body: 'success', status: 200 };
-  }).catch(() => new Error());
+  }).catch(() => {throw new Error()});
 }
