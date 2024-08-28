97b06ed4ebec18884f49c528494060f210bfcef2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 17:32:54 +0000

    Created a floor() function

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+import math
+"""
+2-floor.py
+
+This module contains a function floor() which takes a float n as argument and returns the floor of the float.
 """
 
-"""
+
+def floor(n: float) -> int:
+    """Function floor takes a float and returns the floor of that float
+
+    Args:
+        n (float): A float to floor
+
+    Returns:
+        int: The floor of the float given as an arg
+    """
+    return math.floor(n)
