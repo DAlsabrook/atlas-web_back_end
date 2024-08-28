f581a7dfd3960cd8adbe851ebd65c06f5e346b2f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 17:59:40 +0000

    Added a function to sum a mixed list of float and int

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+Module that contains a type-annotated function sum_mixed_list which takes a
+list mxd_lst of integers and floats and returns their sum as a float.
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """function to get the sum of a list
+
+    Args:
+        mxd_lst (List[int, float]): List to sum
+
+    Returns:
+        float: sum of list
+    """
+    return float(sum(mxd_lst))
