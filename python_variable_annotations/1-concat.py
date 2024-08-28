63b494ae259d4938cb758169fa7658e84663eaec
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 17:29:42 +0000

    Function to concat 2 strings

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+1-concat.py
+
+Module for a function to concat strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat() concats 2 strings
+
+    Args:
+        str1 (str): a string
+        str2 (str): a string
+
+    Returns:
+        str: another string
+    """
+    return str1 + str2
