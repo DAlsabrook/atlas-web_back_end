b681b8da48474bf569fcd6d1907c3bfd1f11116c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 17:54:46 +0000

    Importing the right List from typing. I am using a newer version of python

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 Module that contains a function sum_list which takes a list input_list of
 floats as argument and returns their sum as a float.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Function to get the sum of floats in a list
 
     Args:
@@ -14,4 +15,4 @@ def sum_list(input_list: list[float]) -> float:
     Returns:
         float: Sum of floats
     """
-    return sum(num for num in input_list)
+    return sum(input_list)
