000661a1f98fa67fa36bc0ebc50ce0ec5f3656b3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 17:49:38 +0000

    Function to sum list of floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module that contains a function sum_list which takes a list input_list of
+floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Function to get the sum of floats in a list
+
+    Args:
+        input_list (list[float]): List of floats passed in
+
+    Returns:
+        float: Sum of floats
+    """
+    return sum(num for num in input_list)
