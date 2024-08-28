ee44cb17a21917be74da51be910ea8508df6d283
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 18:12:40 +0000

    typing Tuple import added

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,10 +5,10 @@ an int OR float v as arguments and returns a tuple. The first element of the
 tuple is the string k. The second element is the square of the int/float v and
 should be annotated as a float.
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Converts passed str and float or int to a tuple
 
     Args:
