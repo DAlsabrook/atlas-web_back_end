b5dc3f29eee923dfd2324c2e2a87fababf0335ec
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-28 18:22:01 +0000

    Func to return func with multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+Module that contains a type-annotated function make_multiplier that takes a
+float multiplier as argument and returns a function that multiplies a float
+by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Creates a function with a multiplier
+
+    Args:
+        multiplier (float): Arg to use as multiplier
+
+    Returns:
+        Callable[[float], float]: function to return to caller
+    """
+    def multiplierFunc(num: float) -> float:
+        return num * multiplier
+    return multiplierFunc
+
