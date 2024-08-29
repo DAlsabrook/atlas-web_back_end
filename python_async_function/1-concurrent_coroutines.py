5fac204dfa50e06f35c0e0e55b6f56a5f11e2fc8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-29 03:11:10 +0000

    Pushing function to generate list of delays

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module that contains an asynchronous coroutine that takes in an integer
+argument (max_delay, with a default value of 10) named wait_random that waits
+for a random delay between 0 and max_delay (included and float value) seconds
+and eventually returns it.
+"""
+from asyncio import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Function to generate a list of delays from a function that waits a
+    random amount of time
+
+    Args:
+        n (int): How many times to call the function
+        max_delay (int): max amount of time you want the function to wait
+
+    Returns:
+        List[float]: list of delays used
+    """
+    delay_list = [await wait_random(max_delay) for i in range(0, n)]
+    return delay_list
