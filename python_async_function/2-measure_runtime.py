36b411730c259694019a625c1171ff04bbb6259f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-29 04:24:05 +0000

    Fixing linter errors

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,11 +3,12 @@
 Module that contains an func that measures the total execution time for
 wait_n(n, max_delay), and returns total_time / n. Returns a float.
 """
-import time, asyncio
+import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay:int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the time it takes to run wait_n function
 
     Args:
