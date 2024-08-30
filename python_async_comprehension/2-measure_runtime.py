7506be7787c72659a8a90cb90eecbf6689b110fc
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-30 15:59:59 +0000

    sending in time mesuring funciton, documented, annotated, and linted

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,6 +2,21 @@
 """
 Module that calls an async generator and measures the time it takes to run
 """
+import asyncio
+import time
+async_comprehension = __import__('0-async_comprehension').async_comprehension
 
 
+async def measure_runtime() -> float:
+    """function to measure run time of async generator
 
+    Returns:
+        float: Amount of time that it took to run
+    """
+    startTime = time.time()
+    asyncio.gather(async_comprehension,
+                   async_comprehension,
+                   async_comprehension,
+                   async_comprehension)
+    endTime = time.time()
+    return endTime - startTime
