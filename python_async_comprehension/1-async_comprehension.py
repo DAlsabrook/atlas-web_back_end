70eef8a74452ae5ab08b1158370e0f99e6cea352
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-30 15:47:25 +0000

    Linter issue fixes

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,14 @@
 Module that calls an async generator
 """
 # import asyncio
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """coroutine that get 10 numbers from an async generator
     in an async for
     """
-    return[randNumber async for randNumber in async_generator()]
+    return [randNumber async for randNumber in async_generator()]
 
 # print(asyncio.run(async_comprehension()))
