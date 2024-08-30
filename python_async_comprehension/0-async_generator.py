8890ec7dd6e7eee879f449bdcbb95bcd1d35b74b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-30 15:20:10 +0000

    Annotate the async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,13 +3,13 @@
 Module that contains an async generator
 """
 import asyncio
+from typing import AsyncGenerator
 import random
 
-async def async_generator ():
+async def async_generator () -> AsyncGenerator[float, None]:
     """
     Coroutine that just generates 10 awaits via sleep
     """
     for i in range(10):
         await asyncio.sleep(1)
-
-    yield random.uniform(0, 10)
+        yield random.uniform(0, 10)
