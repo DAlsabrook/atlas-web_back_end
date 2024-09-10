ec26e0b6bd7dfecadd4540e3d6da3f4440e3b2cf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-10 16:25:27 +0000

    forgot to remove test lines and pep8 errors

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -21,22 +21,7 @@ class FIFOCache(BaseCaching):
                 print('DISCARD: {}'.format(firstIn))
                 del self.cache_data[firstIn]
 
-
     def get(self, key):
         """Get a value from the dict or return None if doesnt exist
         """
         return self.cache_data.get(key)
-
-if __name__ == '__main__':
-    my_cache = FIFOCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
