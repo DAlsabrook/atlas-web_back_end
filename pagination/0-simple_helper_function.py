c45527b6816d24c6d47d2cc87a7e407ef39218dd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-10 19:09:13 +0000

    Bringing her down to 1 return statement

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,6 +7,4 @@ Module for a function pagination set up
 def index_range(page, page_size):
     """function to get index of pagination range
     """
-    max = page_size * page
-    min = page_size * (page - 1) if page > 1 else 0
-    return (min, max)
+    return (page_size * (page - 1) if page > 1 else 0, page_size * page)
