945a2a769c7192e848ba88a4207d5251f021039a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-10 16:24:34 +0000

    Changes to adding key to dict and then checking the length

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -15,13 +15,28 @@ class FIFOCache(BaseCaching):
         """Put an item in the object
         """
         if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 firstIn = list(self.cache_data.keys())[0]
                 print('DISCARD: {}'.format(firstIn))
                 del self.cache_data[firstIn]
-            self.cache_data[key] = item
+
 
     def get(self, key):
         """Get a value from the dict or return None if doesnt exist
         """
         return self.cache_data.get(key)
+
+if __name__ == '__main__':
+    my_cache = FIFOCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
