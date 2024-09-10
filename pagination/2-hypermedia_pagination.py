46cf8a9372843e4527c3038360ec4cf8d96e3845
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-10 20:46:47 +0000

    forgot to remove test lines

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -64,14 +64,3 @@ class Server:
             'total_pages': total_pages,
         }
         return pageInfor
-
-
-server = Server()
-
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
