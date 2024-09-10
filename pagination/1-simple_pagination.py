e86e5fcc5d5384cfa2116f14842777e799a944bf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-10 19:59:12 +0000

    Update for task 1. Working locaally

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+"""
+Module for a function pagination set up
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int), """
+        raised when page and/or page_size are not ints
+        """
+        assert page > 0 and page_size > 0, "raised with negative values"
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
+
+
+def index_range(page, page_size):
+    """function to get index of pagination range
+    """
+    return (page_size * (page - 1) if page > 1 else 0, page_size * page)
