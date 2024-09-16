1f5dbb82ef269a14c5bf3499d71152000551a8a3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-16 17:43:16 +0000

    forgot to annotate

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,9 +5,13 @@ idk what this does yet
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """returns a hashed password
     """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
