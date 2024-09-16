f7180302aadd8a4890a603e2149191a589a90bd8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-16 17:41:29 +0000

    pushing first function

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+idk what this does yet
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """returns a hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
