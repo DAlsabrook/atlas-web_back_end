452771a2c2b622f3dbf912b55c23a7e6ff551984
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-17 15:03:49 +0000

    linter

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,10 +11,12 @@ class BasicAuth(Auth):
         """initialize basic auth"""
         super().__init__()
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """returns base64 part of the request header
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if (authorization_header is None
+                or not isinstance(authorization_header, str)):
             return None
         if authorization_header.startswith('Basic'):
             tokenized = authorization_header.split(' ')
