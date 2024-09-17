5435d7b33aae3e0d997e78b35ef85fe1ab72dc2e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-17 15:10:29 +0000

    decode needs the bytes from base64 instead of the actual base64 i guess

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """module for base authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -15,11 +16,28 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """returns base64 part of the request header
         """
-        if (authorization_header is None
-                or not isinstance(authorization_header, str)):
+        if (authorization_header is None or not isinstance(
+            authorization_header, str
+            )):
             return None
         if authorization_header.startswith('Basic'):
             tokenized = authorization_header.split(' ')
             if tokenized[1]:
                 return tokenized[1]
         return None
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """returns base64 part of the request header
+        """
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header,str
+            ):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except:
+            return None
