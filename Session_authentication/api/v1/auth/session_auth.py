cb323c748219b58a44cb0c7fb62288a4bda9580b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-19 19:54:31 +0000

    Added method to session auth class to create the dict to link to a sepcific user_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Module that does nothing besides pass
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -11,4 +12,19 @@ class SessionAuth(Auth):
     Args:
         Auth (_type_): _description_
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """_summary_
+
+        Args:
+            user_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        self.session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[self.session_id] = user_id
+        return self.session_id
