3a3e7ccb8c508fb7806a114caee7f355376e2f4a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 03:03:56 +0000

    getting user out of dict

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,16 @@ class SessionAuth(Auth):
         self.session_id = str(uuid.uuid4())
         self.user_id_by_session_id[self.session_id] = user_id
         return self.session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """_summary_
+
+        Args:
+            session_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
