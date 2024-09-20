0b96d866cc214592a65b2e5d1f56080b2b41c73b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 03:19:10 +0000

    Updated before request and removed previous print lines

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -38,14 +38,13 @@ def before_req():
     # print(f'Request path: {request.path}')
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
+                                            '/api/v1/auth_session/login/',
                                             '/api/v1/forbidden/']):
         return
     auth_header = auth.authorization_header(request)
-    # print(f'Authorization header: {auth_header}')
-    if auth_header is None:
+    if auth_header is None and auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
-    # print(f'Current user: {current_user}')
     if request.current_user is None:
         abort(403)
 
