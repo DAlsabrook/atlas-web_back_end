84226f5453a28b1f0cbe19af81f583d4671c3942
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 04:01:19 +0000

    Hoping i got all of this method right. Added route for session login

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+"""
+Routes for session auth
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    if not email:
+        return jsonify({ "error": "email missing" }), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({ "error": "password missing" }), 400
+    user = User.search({"email": email})
+    if not user:
+        return jsonify({ "error": "no user found for this email" }), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "invalid credentials"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = user.to_json()
+    response.set_cookie(os.getenv('SESSION_NAME', '_my_session_id'), session_id)
+    return response
