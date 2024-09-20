7d8ce9fbb416fd37d7cd6e0c8f6c8f725d1eb67c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 16:58:00 +0000

    linter issues

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def status() -> str:
     """ GET /
@@ -13,6 +14,7 @@ def status() -> str:
       - JSON payload with a welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
