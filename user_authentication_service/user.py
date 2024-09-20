e7ee0467344eed5638f8450f3f8a8fed13346d40
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 14:50:06 +0000

    Adding the declarative base as inherted

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class User():
+class User(Base):
     """User class for the user table
 
     Returns:
@@ -15,7 +17,3 @@ class User():
     hashed_password = Column(String, nullable=False)
     session_id = Column(String)
     reset_token = Column(String)
-
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-           self.name, self.fullname, self.nickname)
