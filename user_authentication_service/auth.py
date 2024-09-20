874dce78dd99e3905534f77f87073516f5e82da7
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 16:43:52 +0000

    Added method to add user to db and hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,33 @@
 Module to handle auth to get things from the DB
 """
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register user to the db
+
+        Args:
+            email (str): user email
+            password (str): user password
+
+        Returns:
+            User: user object
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {email} already exists")
+        hashed_pass = _hash_password(password)
+        user = self._db.add_user(email, hashed_pass)
+        return user
 
 
 def _hash_password(password: str) -> bytes:
