f85299fbb9219f4dfb105d4bc4dfdf15fbc050e7
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-20 04:03:33 +0000

    linter issues

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """
 Routes for session auth
 """
@@ -17,14 +17,14 @@ def login() -> str:
     """
     email = request.form.get('email')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
 
     password = request.form.get('password')
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     user = User.search({"email": email})
     if not user:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "invalid credentials"}), 401
@@ -32,5 +32,6 @@ def login() -> str:
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = user.to_json()
-    response.set_cookie(os.getenv('SESSION_NAME', '_my_session_id'), session_id)
+    response.set_cookie(os.getenv('SESSION_NAME',
+                                  '_my_session_id'), session_id)
     return response
