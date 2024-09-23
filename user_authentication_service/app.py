fd76d19a64888bce492a5665408853be2c146556
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-23 03:29:21 +0000

    Strict slashes?

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -34,7 +34,7 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
     """Login function to respond to POST /sessions"""
     email = request.form.get('email')
