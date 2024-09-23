1e592cddee869ef4cf0c6312e5242d1c41daf903
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-23 04:28:15 +0000

    linter issues

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -111,7 +111,6 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
-
     def update_password(self, reset_token: str, password: str) -> None:
         """Update the user's password using the reset token
 
