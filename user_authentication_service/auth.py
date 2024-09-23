4608ad9ede81fe830d6348fada135bdaf9d70556
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-23 03:47:07 +0000

    Update user reset token method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -94,6 +94,23 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """get reset token
+
+        Args:
+            email (str): User email
+
+        Returns:
+            str: UUID reset token
+        """
+        user = self._db.find_user_by(email=email)
+
+        if user is None:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
 
 def _generate_uuid() -> str:
     """Generate a new UUID and return its string representation.
