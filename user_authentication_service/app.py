f327d835f1e7a045fc478d23591379c9d76831e8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-23 04:06:43 +0000

    Removed a check i think might be failing this task

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -89,8 +89,6 @@ def profile():
 def get_reset_password_token():
     """Login function to respond to POST /reset_password"""
     email = request.form.get('email')
-    if not email:
-      abort(403)
 
     try:
         reset_token = AUTH.get_reset_password_token(email)
