79cf9db4b2017854dcf4e50648fa7ac025b522dd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-23 03:48:49 +0000

    changed to try/except to catch error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -103,14 +103,13 @@ class Auth:
         Returns:
             str: UUID reset token
         """
-        user = self._db.find_user_by(email=email)
-
-        if user is None:
-            raise ValueError
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
-
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except Exception:
+            ValueError
 
 def _generate_uuid() -> str:
     """Generate a new UUID and return its string representation.
