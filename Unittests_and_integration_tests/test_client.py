ee998b5ac5aa8d3c9df8ba34ae645b8364a01ee9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-25 15:28:45 +0000

    testing public repos

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -29,6 +29,17 @@ class TestGithubOrgClient(unittest.TestCase):
         result = githubOrg._public_repos_url
         self.assertEqual('https://api.github.com/orgs/google/repos', result)
 
+    @patch('client.get_json', return_value=[{'name': 'repo1'}, {'name': 'repo2'}])
+    def test_public_repos(self, mock_get_json):
+        """Testing GithubOrgClient.public_repos"""
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = 'https://api.github.com/orgs/google/repos'
+            githubOrg = GithubOrgClient('google')
+            result = githubOrg.public_repos()
+            self.assertEqual(result, ['repo1', 'repo2'])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with('https://api.github.com/orgs/google/repos')
+
 
 if __name__ == "__main__":
     unittest.main()
