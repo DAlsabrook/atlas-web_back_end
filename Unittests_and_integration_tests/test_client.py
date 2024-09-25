4d899df8f19f7942440af48d9ec47e0ef1c24e83
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-25 15:05:12 +0000

    Switched to mocking the memoize property with PropertyMock

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,19 +15,19 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json', return_value={'login': 'orgName'})
     def test_org(self, org, expected, getJson):
-        """Test that GithubOrgClient.org returns the correct value"""
+        """Test GithubOrgClient.org"""
         githubOrg = GithubOrgClient(org)
         result = githubOrg.org
         getJson.assert_called_once_with(f'https://api.github.com/orgs/{org}')
         self.assertEqual(result, expected)
 
-    @patch('client.GithubOrgClient.org', return_value={'https://api.github.com/orgs/google'})
-    def test_public_repos_url(self):
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mockOrg):
         """Testing githubOrgClient._public_repos_url"""
+        mockOrg.return_value = {'repos_url': 'https://api.github.com/orgs/google/repos'}
         githubOrg = GithubOrgClient('google')
-        mockedResult = githubOrg.org()
-        result = githubOrg._public_repos_url()
-        self.asserEqual(mockedResult, result)
+        result = githubOrg._public_repos_url
+        self.assertEqual('https://api.github.com/orgs/google/repos', result)
 
 
 if __name__ == "__main__":
