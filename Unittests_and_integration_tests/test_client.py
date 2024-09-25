013c682c3bc999b7a9c4af137709c81fcc6590a1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-09-25 15:31:30 +0000

    Reworked function for linter and made it way less readable

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -24,21 +24,26 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mockOrg):
         """Testing githubOrgClient._public_repos_url"""
-        mockOrg.return_value = {'repos_url': 'https://api.github.com/orgs/google/repos'}
+        mockOrg.return_value = {'repos_url':
+                                'https://api.github.com/orgs/google/repos'}
         githubOrg = GithubOrgClient('google')
         result = githubOrg._public_repos_url
         self.assertEqual('https://api.github.com/orgs/google/repos', result)
 
-    @patch('client.get_json', return_value=[{'name': 'repo1'}, {'name': 'repo2'}])
+    @patch('client.get_json',
+           return_value=[{'name': 'repo1'}, {'name': 'repo2'}])
     def test_public_repos(self, mock_get_json):
         """Testing GithubOrgClient.public_repos"""
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = 'https://api.github.com/orgs/google/repos'
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mockRepos:
+            mockRepos.return_value = "https://api.github.com/orgs/google/repos"
             githubOrg = GithubOrgClient('google')
             result = githubOrg.public_repos()
             self.assertEqual(result, ['repo1', 'repo2'])
-            mock_public_repos_url.assert_called_once()
-            mock_get_json.assert_called_once_with('https://api.github.com/orgs/google/repos')
+            mockRepos.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                'https://api.github.com/orgs/google/repos'
+                )
 
 
 if __name__ == "__main__":
