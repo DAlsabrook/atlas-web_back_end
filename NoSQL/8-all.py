fc8ed0200604d05d1190052f27044462f687afe2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 19:40:44 +0000

    test checker for python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,8 @@
 #!/usr/bin/end python3
+""" Module for method to list documents
+"""
+
+def list_all(mongo_collection):
+    """List all documents in collection
+    """
+    return mongo_collection.find()
