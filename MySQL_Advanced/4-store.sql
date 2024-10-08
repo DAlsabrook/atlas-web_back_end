69ed11c7c8f3c5f4debe58fc13c7bb91974a480a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 15:28:02 +0000

    I think this is how you do a trigger

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,9 @@
+-- creates a trigger that decreases the quantity of an item after new order.
+
+CREATE TRIGGER update_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET items.quantity = items.quantity - NEW.number
+    WHERE items.name = NEW.item_name;
+END;
