fa553180a160902e53155d71894d56080b70bb13
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 00:56:33 +0000

    FORGOT to filter by glam rock...

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- Get bands and order by longevity
 
 SELECT band_name, COALESCE(split, YEAR(CURDATE())) - formed AS lifespan FROM metal_bands
+WHERE style LIKE "%Glam Rock%"
 ORDER BY lifespan DESC
-LIMIT 1;
