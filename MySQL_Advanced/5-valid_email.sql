64e9459aafa4fb3408a18fd6babf2a0404db257d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 15:59:37 +0000

    update from != to <>

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ delimiter //
 CREATE TRIGGER update_valid_email AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email THEN
+    IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     ELSE
         SET NEW.valid_email = 1;
