a42c2763e0d57619bf20f97cf29de97df9de096c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 17:11:14 +0000

    storing id in a variable i guess

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -2,13 +2,13 @@
 
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id int
+    user_id int
 )
 BEGIN
     DECLARE score_average FLOAT;
-
+    DECLARE passed_id INT = user_id;
     SELECT AVG(score) INTO score_average FROM corrections
-    WHERE user_id = IN user_id;
+    WHERE user_id = passed_id;
 
     UPDATE users
     SET average_score = score_average
