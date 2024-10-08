f888bb67df6cce0dd9106c65e340d92004937fd2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 16:00:12 +0000

    BEFORE instead of AFTER?

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- creates a trigger that resets valid_email when the email has been changed
 
 delimiter //
-CREATE TRIGGER update_valid_email AFTER UPDATE ON users
+CREATE TRIGGER update_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
