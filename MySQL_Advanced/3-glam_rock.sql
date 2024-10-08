e3787abb8964bdaf7b9af5d8710c36b77ab90a23
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 00:34:04 +0000

    First attempt at subtraction and using current date for NULL split years

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Column names must be: band_name and lifespan (in years)
+-- Get bands and order by longevity
+
+SELECT band_name, COALESCE(split, YEAR(CURDATE())) - formed AS lifespan FROM metal_bands
+ORDER BY lifespan DESC;
