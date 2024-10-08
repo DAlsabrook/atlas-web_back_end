1f68ba5d640d6f609d712dc9201dbb6d6919a951
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-08 15:29:04 +0000

    Added delimeter to allow for multiple semicolons in the script

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,9 +1,11 @@
 -- creates a trigger that decreases the quantity of an item after new order.
 
+delimiter //
 CREATE TRIGGER update_quantity AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET items.quantity = items.quantity - NEW.number
-    WHERE items.name = NEW.item_name;
-END;
+    BEGIN
+        UPDATE items
+        SET items.quantity = items.quantity - NEW.number
+        WHERE items.name = NEW.item_name;
+    END//
+delimiter ;
