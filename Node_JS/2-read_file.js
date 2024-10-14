2d3c8250e498a4e428d703808a4831027c724bf9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-14 23:31:19 +0000

    Working. That was hard. Passes linter also

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -1,20 +1,35 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    const file = fs.readFileSync(path, 'utf8', (err, data) => {
-        if (err) {
-            process.stdout('Cannot load the database')
-        };
-    }).trim();
-    let lineCount = -1; // Removes column description line
-    let studentsInField = {};
-    file.split('\n').forEach(line => {
-        lineCount += 1;
-        const column = line.split(',');
-        process.stdout.write(`\tColumn: ${column[1]}\n`)
-    })
-    process.stdout.write(`Number of students: ${lineCount.toString()}\n`);
+  const file = fs.readFileSync(path, 'utf8', (err) => {
+    if (err) {
+      process.stdout('Cannot load the database');
+    }
+  }).trim();
+  let lineCount = 0;
+  const studentsInField = {};
+  const fileLines = file.split('\n');
+  fileLines.slice(1).forEach((line) => {
+    lineCount += 1;
+    const column = line.split(',');
+    const field = column[3].trim();
+    const firstName = column[0].trim();
+    if (!studentsInField[field]) {
+      studentsInField[field] = [];
+    }
+    studentsInField[field].push(firstName);
+  });
 
+  // process.stdout.write(JSON.stringify(studentsInField) + "\n")
+  for (const [field, studentsList] of Object.entries(studentsInField)) {
+    process.stdout.write(`Number if students in ${
+      field
+    }: ${
+      studentsList.length
+    }. List: ${
+      studentsList.join(', ')}\n`);
+  }
+  process.stdout.write(`Number of students: ${lineCount.toString()}\n`);
 }
-countStudents('./database.csv')
+countStudents('./database.csv');
 module.exports = countStudents;
