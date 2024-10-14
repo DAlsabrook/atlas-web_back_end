b2575740b23c8d75a0d2fd32048117504df25c45
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-14 17:24:50 +0000

    Pushing task 1, works locally and with child process

--- Node_JS/0-console.js
+++ Node_JS/0-console.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {string} message String to print to stdout
+ */
+
 function displayMessage(message) {
     // Write to STDOUT
     process.stdout.write(message);
---
+++ Node_JS/1-stdin.js
@@ -0,0 +1,8 @@
+
+process.stdout.write('Welcome to Atlas School, what is your name?\n');
+process.stdin.on('data', data => {
+    data = data.toString().trim();
+    process.stdout.write(`Your name is: ${data}\n`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+});
