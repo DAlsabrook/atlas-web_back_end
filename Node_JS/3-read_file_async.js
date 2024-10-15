19c1e6c625b1fe592625c0cc8283c6b900084f4b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-15 00:42:56 +0000

    Copy task 2 and delete 1 word?

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    let lineCount = 0;
+    const studentsInField = {};
+
+    const file = fs.readFile(path, 'utf8', (err) => {
+        if (err) {
+            process.stdout('Cannot load the database');
+        }
+    }).trim();
+    const fileLines = file.split('\n');
+
+    fileLines.slice(1).forEach((line) => {
+        lineCount += 1;
+        const column = line.split(',');
+        const field = column[3].trim();
+        const firstName = column[0].trim();
+        if (!studentsInField[field]) {
+            studentsInField[field] = [];
+        }
+        studentsInField[field].push(firstName);
+    });
+
+    for (const [field, studentsList] of Object.entries(studentsInField)) {
+        process.stdout.write(`Number of students in ${field
+            }: ${studentsList.length
+            }. List: ${studentsList.join(', ')}\n`);
+    }
+    process.stdout.write(`Number of students: ${lineCount.toString()}\n`);
+}
+
+countStudents('./database.csv');
+module.exports = countStudents;
