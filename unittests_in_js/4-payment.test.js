13cbe3f0b48f292d87c5bcb6d88b6df97c2ac211
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-22 16:37:57 +0000

    Got it working locally

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,11 +1,13 @@
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 const Utils = require('./utils.js');
 const assert = require('assert');
 
 describe("sendPaymentRequestToApi", function () {
+    let consoleSpy;
+
     beforeEach(function () {
-        sinon.spy(Utils, "calculateNumber");
+        consoleSpy = sinon.spy(console, 'log');
     });
 
     afterEach(function () {
@@ -13,12 +15,22 @@ describe("sendPaymentRequestToApi", function () {
     });
 
     it("check if calculateNumber is called and called correctly", function () {
-        const consoleSpy = sinon.spy(console, 'log');
+        const calculateNumberSpy = sinon.spy(Utils, "calculateNumber");
         sendPaymentRequestToApi(100, 20);
         // make sure the calculateNumber method is called with right params
-        assert(Utils.calculateNumber.calledWith('SUM', 100, 20));
+        assert(calculateNumberSpy.calledWith('SUM', 100, 20));
         // check that the console has the correct output
         assert(consoleSpy.calledWith('The total is: 120'));
-        consoleSpy.restore();
+    });
+
+    it("Check correct output using stub on calculateNumber", function () {
+        const stub = sinon.stub(Utils, 'calculateNumber')
+        .withArgs("SUM", 100, 20)
+        .returns(10);
+        sendPaymentRequestToApi(100, 20);
+        // make sure the calculateNumber method is called with right params
+        assert(stub.calledWith('SUM', 100, 20));
+        // check that the console has the correct output with the stub output
+        assert(consoleSpy.calledWith('The total is: 10'));
     });
 });
