eb1a4438ee29a6488350c0b54ac5311df5b05ecb
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-22 19:35:15 +0000

    copied and renamed folder

---
+++ unittests_in_js/10-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const assert = require('assert');
+
+describe('Index Page', () => {
+    it('should return status code 200', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            assert.strictEqual(response.statusCode, 200);
+            done();
+        });
+    });
+
+    it('should return the correct message', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            assert.strictEqual(body, 'Welcome to the payment system');
+            done();
+        });
+    });
+
+    it('should return the correct message with id', (done) => {
+        request('http://localhost:7865/cart/123', (error, response, body) => {
+            assert.strictEqual(body, 'Payment methods for cart 123');
+            done();
+        });
+    });
+
+    it('id is not a number', (done) => {
+        request('http://localhost:7865/cart/abc', (error, response, body) => {
+            assert.strictEqual(body, 'id must be a number');
+            done();
+        });
+    });
+});
