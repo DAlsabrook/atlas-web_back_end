00e3fff90b409bba2792bcb63607e98dd2510a37
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-22 19:13:11 +0000

    switching to old and depreciated http "request" module

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,15 +1,18 @@
-const request = require('supertest');
+const request = require('request');
+const assert = require('assert');
 
 describe('Index Page', () => {
     it('should return status code 200', (done) => {
-        request('http://localhost:7865')
-            .get('/')
-            .expect(200, done);
+        request('http://localhost:7865', (error, response, body) => {
+            assert.strictEqual(response.statusCode, 200);
+            done();
+        });
     });
 
     it('should return the correct message', (done) => {
-        request('http://localhost:7865')
-            .get('/')
-            .expect('Welcome to the payment system', done);
+        request('http://localhost:7865', (error, response, body) => {
+            assert.strictEqual(body, 'Welcome to the payment system');
+            done();
+        });
     });
 });
