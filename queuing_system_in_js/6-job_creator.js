81f57faae1e8d824892e7ebad3a56cd32ac44b6e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-23 16:58:49 +0000

    idk what i changed

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -8,13 +8,13 @@ const jobData = {
 };
 
 const job = queue.create('push_notification_code', jobData)
-    .save((err) => {
-        if (!err) {
-            console.log(`Notification job created: ${job.id}`);
-        } else {
-            console.error('Error creating job:', err);
-        }
-    });
+.save((err) => {
+    if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+    } else {
+        console.error('Error creating job:', err);
+    }
+});
 
 job.on('complete', () => {
     console.log('Notification job completed');
