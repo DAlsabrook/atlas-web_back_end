12c8a6f378f91e45a3a117fc0cbe7312b4471fe2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-23 03:33:58 +0000

    removed practice and idk how to make this work

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -12,15 +12,5 @@ client.on('error', (err) => {
     console.error('Redis client not connected to the server: ', err);
 });
 
-// Set and get a value to verify the connection
-client.set('Holberton', 'School', redis.print);
-client.get('Holberton', (err, reply) => {
-    if (err) {
-        console.error('Error getting value:', err);
-    } else {
-        console.log('Value:', reply); // Should print "School"
-    }
-});
-
 // Close the connection
 client.quit();
