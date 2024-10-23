4877e8b45655831d868cd201b968c40c655bbbd1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-23 16:58:07 +0000

    job create in que

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '2813308004',
+    message: 'Hello, from Mike Jones.',
+};
+
+const job = queue.create('push_notification_code', jobData)
+    .save((err) => {
+        if (!err) {
+            console.log(`Notification job created: ${job.id}`);
+        } else {
+            console.error('Error creating job:', err);
+        }
+    });
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+}).on('failed', () => {
+    console.log('Notification job failed');
+});
